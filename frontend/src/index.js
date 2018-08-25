d1f3a4f852882fdedef4205d6d3914fd5d29d24b
mwj777 <mjohnson324@gmail.com>
2018-08-25 16:25:22 -0400

    Reinitialize frontend with linnia-box

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Favicon from 'linnia-brand/Favicon';
+import store from './store';
+import AppContainer from './containers/AppContainer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+/*
+  Remove this ThemeProvider and insert your own to quickly restyle the application!
+*/
+
+import ThemeProvider from 'linnia-brand/ThemeProvider';
+
+/*
+  You can read more about creating your own theme here: https://material-ui.com/customization/themes/
+*/
+
+ReactDOM.render(
+  <Fragment>
+    <Favicon />
+    <Provider store={store}>
+      <CssBaseline>
+        <ThemeProvider>
+          <AppContainer />
+        </ThemeProvider>
+      </CssBaseline>
+    </Provider>
+  </Fragment>, document.getElementById('root'));
